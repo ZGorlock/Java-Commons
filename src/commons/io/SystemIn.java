f4c60abd711739e41379e0c0930a3d6b116fe4df
Zachary Gill <zachary.w.gill@gmail.com>
2021-06-15 05:02:17 -0400

    Updated SystemIn

--- src/commons/io/SystemIn.java
+++ src/commons/io/SystemIn.java
@@ -11,7 +11,12 @@ import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicReference;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,19 +46,19 @@ public final class SystemIn extends SingletonInputHandler {
     private static final Scanner in = new Scanner(systemIn);
     
     /**
-     * The console input object.
-     */
-    private static final Console console = System.console();
-    
-    /**
      * The buffer for the scanner.
      */
-    private static String buffer = null;
+    private static AtomicReference<String> buffer = null;
     
     /**
      * The thread that scans for input.
      */
-    private static Thread scannerThread = null;
+    private static ExecutorService scannerThread = null;
+    
+    /**
+     * The handle for the thread that scans for input.
+     */
+    private static Future<?> scannerThreadHandle = null;
     
     /**
      * The singleton instance of the Input Handler.
@@ -79,36 +84,54 @@ public final class SystemIn extends SingletonInputHandler {
     @SuppressWarnings("BusyWait")
     private static synchronized void startScanner() {
         interruptScanner();
-        scannerThread = new Thread(() -> {
+        
+        scannerThread = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setNameFormat("SystemIn").build());
+        scannerThreadHandle = scannerThread.submit(() -> {
             try {
-                while (systemIn.available() == 0) {
+                while ((systemIn.available() == 0) || !in.hasNextLine()) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        throw new InterruptedException();
+                    }
                     Thread.sleep(5);
                 }
-                in.hasNextLine();
-                buffer = in.nextLine();
-            } catch (IOException | InterruptedException ignored) {
-                buffer = "";
+                buffer = new AtomicReference<>(in.nextLine());
+            } catch (IOException | InterruptedException | IllegalStateException ignored) {
+                buffer = new AtomicReference<>("");
             }
-        }, "SystemIn");
-        scannerThread.start();
+        });
     }
     
     /**
      * Interrupts the scanner for System.in.
      */
+    @SuppressWarnings("StatementWithEmptyBody")
     private static synchronized void interruptScanner() {
-        if ((scannerThread != null) && scannerThread.isAlive()) {
-            scannerThread.interrupt();
-            try {
-                scannerThread.join();
-            } catch (InterruptedException ignored) {
+        if (isScannerRunning()) {
+            scannerThreadHandle.cancel(true);
+            scannerThread.shutdown();
+            scannerThread.shutdownNow();
+            while (!scannerThread.isShutdown()) {
             }
+            scannerThread = null;
+            scannerThreadHandle = null;
         }
+        
         buffer = null;
     }
     
     /**
-     * Gets the next line of input from the scanner.
+     * Determines if the scanner thread is running or not.
+     *
+     * @return Whether the scanner thread is running or not.
+     */
+    private static synchronized boolean isScannerRunning() {
+        return ((scannerThread != null) && (scannerThreadHandle != null) &&
+                !scannerThread.isShutdown() && !scannerThread.isTerminated() &&
+                !scannerThreadHandle.isDone() && !scannerThreadHandle.isCancelled());
+    }
+    
+    /**
+     * Gets the next line of input from the scanner, blocks the calling thread until the next line is retrieved.
      *
      * @param caller The calling class.
      * @return The next line of input, or an empty string if the caller is not the owner of the Input Handler.
@@ -120,7 +143,7 @@ public final class SystemIn extends SingletonInputHandler {
         }
         
         startScanner();
-        while (buffer == null) {
+        while (isScannerRunning() && (buffer == null)) {
             try {
                 Thread.sleep(5);
             } catch (InterruptedException ignored) {
@@ -141,25 +164,23 @@ public final class SystemIn extends SingletonInputHandler {
     }
     
     /**
-     * Gets a password input from the console.
+     * Gets a password input from the console, privately if there is a console, or not privately otherwise.
      *
      * @param caller The calling class.
      * @return The password input, or an empty string if the caller is not the owner of the Input Handler.
-     * @see Console#readPassword()
+     * @see PasswordIn#readPassword(Class)
      */
     public static synchronized String getPassword(Class<?> caller) {
         if (!owns(caller)) {
             return "";
         }
         
-        if ((scannerThread != null) && scannerThread.isAlive()) {
-            scannerThread.interrupt();
-        }
-        return (console != null) ? String.valueOf(console.readPassword()) : nextLine(caller);
+        interruptScanner();
+        return PasswordIn.hasConsole() ? PasswordIn.readPassword(caller) : nextLine(caller);
     }
     
     /**
-     * Gets a password input from the console.
+     * Gets a password input from the console, privately if there is a console, or not privately otherwise.
      *
      * @param caller The calling object.
      * @return The password input, or an empty string if the caller is not the owner of the Input Handler.
@@ -181,15 +202,9 @@ public final class SystemIn extends SingletonInputHandler {
         }
         
         if (buffer != null) {
-            String line = buffer;
+            String line = buffer.getAndSet("");
             buffer = null;
-            if (instance.owner.equals(instance.defaultOwner) && !scannerThread.isAlive()) {
-                startScanner();
-            }
             return line;
-        }
-        if (instance.owner.equals(instance.defaultOwner) && ((scannerThread == null) || !scannerThread.isAlive())) {
-            startScanner();
         }
         return null;
     }
@@ -291,6 +306,71 @@ public final class SystemIn extends SingletonInputHandler {
      */
     public static synchronized boolean relinquish(Object owner) {
         return instance.relinquishOwnership(owner);
+    }
+    
+    
+    //Inner Classes
+    
+    /**
+     * Handles the reading of passwords from System.in.
+     */
+    protected static final class PasswordIn {
+        
+        //Static Fields
+        
+        /**
+         * The console input object.
+         */
+        private static final Console console = System.console();
+        
+        
+        //Functions
+        
+        /**
+         * Reads a password from the console.
+         *
+         * @return The password.
+         * @see Console#readPassword()
+         */
+        private static char[] doReadPassword() {
+            return console.readPassword();
+        }
+        
+        /**
+         * Reads a password from the console.
+         *
+         * @param caller The calling class.
+         * @return The password.
+         * @see #doReadPassword()
+         */
+        public static String readPassword(Class<?> caller) {
+            if (!owns(caller)) {
+                return "";
+            }
+            
+            return hasConsole() ? String.valueOf(doReadPassword()) : "";
+        }
+        
+        /**
+         * Reads a password from the console.
+         *
+         * @param caller The calling object.
+         * @return The password.
+         * @see #readPassword(Class)
+         */
+        public static String readPassword(Object caller) {
+            return readPassword((caller != null) ? caller.getClass() : null);
+        }
+        
+        /**
+         * Determines if the system has a console or not.
+         *
+         * @return Whether the system has a console or not.
+         */
+        public static boolean hasConsole() {
+            return (console != null);
+        }
+        
     }
     
 }
