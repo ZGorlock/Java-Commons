1369d440add74de7944240f5319830350597c3e3
Zachary Gill <zachary.w.gill@gmail.com>
2022-03-08 17:07:31 -0500

    Added CheckedBinaryOperator

---
+++ src/commons/lambda/function/CheckedBinaryOperator.java
@@ -0,0 +1,49 @@
+/*
+ * File:    CheckedBinaryOperator.java
+ * Package: commons.lambda.function
+ * Author:  Zachary Gill
+ * Repo:    https://github.com/ZGorlock/Java-Commons
+ */
+
+package commons.lambda.function;
+
+import java.util.function.BinaryOperator;
+
+/**
+ * A lambda function that accepts two operands and tries to produce a result of the same type and ignores errors.
+ *
+ * @param <T> The type of the operands and the result.
+ */
+@FunctionalInterface
+public interface CheckedBinaryOperator<T> extends BinaryOperator<T> {
+    
+    //Methods
+    
+    /**
+     * Tries to produce a result from the operands.
+     *
+     * @param op1 The first operand.
+     * @param op2 The second operand.
+     * @return The result.
+     * @throws Throwable When there is an error.
+     */
+    T tryApply(T op1, T op2) throws Throwable;
+    
+    /**
+     * Tries to produce a result from the operands and ignores errors.
+     *
+     * @param op1 The first operand.
+     * @param op2 The second operand.
+     * @return The result, or null if there was an error.
+     * @see BinaryOperator#apply(Object, Object)
+     * @see #tryApply(Object, Object)
+     */
+    default T apply(T op1, T op2) {
+        try {
+            return tryApply(op1, op2);
+        } catch (Throwable ignored) {
+            return null;
+        }
+    }
+    
+}
---
+++ test/commons/lambda/function/CheckedBinaryOperatorTest.java
@@ -0,0 +1,119 @@
+/*
+ * File:    CheckedBinaryOperatorTest.java
+ * Package: commons.lambda.function
+ * Author:  Zachary Gill
+ * Repo:    https://github.com/ZGorlock/Java-Commons
+ */
+
+package commons.lambda.function;
+
+import commons.test.TestUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JUnit test of CheckedBinaryOperator.
+ *
+ * @see CheckedBinaryOperator
+ */
+@SuppressWarnings({"RedundantSuppression", "ConstantConditions", "unchecked", "SpellCheckingInspection"})
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CheckedBiFunction.class})
+public class CheckedBinaryOperatorTest {
+    
+    //Logger
+    
+    /**
+     * The logger.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(CheckedBinaryOperatorTest.class);
+    
+    
+    //Initialization
+    
+    /**
+     * The JUnit class setup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @BeforeClass
+    public static void setupClass() throws Exception {
+    }
+    
+    /**
+     * The JUnit class cleanup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @AfterClass
+    public static void cleanupClass() throws Exception {
+    }
+    
+    /**
+     * The JUnit setup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @Before
+    public void setup() throws Exception {
+    }
+    
+    /**
+     * The JUnit cleanup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @After
+    public void cleanup() throws Exception {
+    }
+    
+    
+    //Tests
+    
+    /**
+     * JUnit test of constants.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @Test
+    public void testConstants() throws Exception {
+    }
+    
+    /**
+     * JUnit test of tryApply.
+     *
+     * @throws Exception When there is an exception.
+     * @see CheckedBinaryOperator#tryApply(Object, Object)
+     */
+    @Test
+    public void testTryApply() throws Exception {
+        //standard
+        TestUtils.assertMethodExists(CheckedBinaryOperator.class, "tryApply", Object.class, Object.class);
+    }
+    
+    /**
+     * JUnit test of apply.
+     *
+     * @throws Exception When there is an exception.
+     * @see CheckedBinaryOperator#apply(Object, Object)
+     */
+    @Test
+    public void testApply() throws Exception {
+        //standard
+        TestUtils.assertMethodExists(CheckedBinaryOperator.class, "apply", Object.class, Object.class);
+    }
+    
+}
