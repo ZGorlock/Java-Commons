2644ae9e99016fc7444c7d2e64500c46a8412a32
Zachary Gill <zachary.w.gill@gmail.com>
2022-03-08 15:49:41 -0500

    Added CheckedBiConsumer

---
+++ src/commons/lambda/function/CheckedBiConsumer.java
@@ -0,0 +1,47 @@
+/*
+ * File:    CheckedBiConsumer.java
+ * Package: commons.lambda.function
+ * Author:  Zachary Gill
+ * Repo:    https://github.com/ZGorlock/Java-Commons
+ */
+
+package commons.lambda.function;
+
+import java.util.function.BiConsumer;
+
+/**
+ * A lambda function that tries to consume two inputs and returns no result and ignores errors.
+ *
+ * @param <T> The type of the first input.
+ * @param <U> The type of the second input.
+ */
+@FunctionalInterface
+public interface CheckedBiConsumer<T, U> extends BiConsumer<T, U> {
+    
+    //Methods
+    
+    /**
+     * Tries to consume the inputs.
+     *
+     * @param arg1 The first input.
+     * @param arg2 The second input.
+     * @throws Throwable When there is an error.
+     */
+    void tryAccept(T arg1, U arg2) throws Throwable;
+    
+    /**
+     * Tries to consume the inputs and ignores errors.
+     *
+     * @param arg1 The first input.
+     * @param arg2 The second input.
+     * @see BiConsumer#accept(Object, Object)
+     * @see #tryAccept(Object, Object)
+     */
+    default void accept(T arg1, U arg2) {
+        try {
+            tryAccept(arg1, arg2);
+        } catch (Throwable ignored) {
+        }
+    }
+    
+}
---
+++ test/commons/lambda/function/CheckedBiConsumerTest.java
@@ -0,0 +1,119 @@
+/*
+ * File:    CheckedBiConsumerTest.java
+ * Package: commons.lambda.function
+ * Author:  Zachary Gill
+ * Repo:    https://github.com/ZGorlock/Java-Commons
+ */
+
+package commons.lambda.function;
+
+import commons.test.TestUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JUnit test of CheckedBiConsumer.
+ *
+ * @see CheckedBiConsumer
+ */
+@SuppressWarnings({"RedundantSuppression", "ConstantConditions", "unchecked", "SpellCheckingInspection"})
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CheckedBiConsumer.class})
+public class CheckedBiConsumerTest {
+    
+    //Logger
+    
+    /**
+     * The logger.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(CheckedBiConsumerTest.class);
+    
+    
+    //Initialization
+    
+    /**
+     * The JUnit class setup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @BeforeClass
+    public static void setupClass() throws Exception {
+    }
+    
+    /**
+     * The JUnit class cleanup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @AfterClass
+    public static void cleanupClass() throws Exception {
+    }
+    
+    /**
+     * The JUnit setup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @Before
+    public void setup() throws Exception {
+    }
+    
+    /**
+     * The JUnit cleanup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @After
+    public void cleanup() throws Exception {
+    }
+    
+    
+    //Tests
+    
+    /**
+     * JUnit test of constants.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @Test
+    public void testConstants() throws Exception {
+    }
+    
+    /**
+     * JUnit test of tryAccept.
+     *
+     * @throws Exception When there is an exception.
+     * @see CheckedBiConsumer#tryAccept(Object, Object)
+     */
+    @Test
+    public void testTryAccept() throws Exception {
+        //standard
+        TestUtils.assertMethodExists(CheckedBiConsumer.class, "tryAccept", Object.class, Object.class);
+    }
+    
+    /**
+     * JUnit test of accept.
+     *
+     * @throws Exception When there is an exception.
+     * @see CheckedBiConsumer#accept(Object, Object)
+     */
+    @Test
+    public void testAccept() throws Exception {
+        //standard
+        TestUtils.assertMethodExists(CheckedBiConsumer.class, "accept", Object.class, Object.class);
+    }
+    
+}
