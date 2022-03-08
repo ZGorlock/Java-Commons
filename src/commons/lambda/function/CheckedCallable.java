3060b3c6b6e15c72fdb5ffcb9421a90639bbd4f5
Zachary Gill <zachary.w.gill@gmail.com>
2022-03-08 16:00:53 -0500

    Added CheckedCallable

---
+++ src/commons/lambda/function/CheckedCallable.java
@@ -0,0 +1,46 @@
+/*
+ * File:    CheckedCallable.java
+ * Package: commons.lambda.function
+ * Author:  Zachary Gill
+ * Repo:    https://github.com/ZGorlock/Java-Commons
+ */
+
+package commons.lambda.function;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Represents a lambda function that run a task and returns a result and ignores exceptions.
+ *
+ * @param <T> The type of the result.
+ */
+@FunctionalInterface
+public interface CheckedCallable<T> extends Callable<T> {
+    
+    //Methods
+    
+    /**
+     * Tries to run the task.
+     *
+     * @return The result.
+     * @throws Exception When there is an exception.
+     */
+    T tryCall() throws Exception;
+    
+    /**
+     * Tries to run the task and ignores errors.
+     *
+     * @return The result, or null if there was an error.
+     * @throws Exception This will not throw an exception.
+     * @see Callable#call()
+     * @see #tryCall()
+     */
+    default T call() throws Exception {
+        try {
+            return tryCall();
+        } catch (Throwable ignored) {
+            return null;
+        }
+    }
+    
+}
---
+++ test/commons/lambda/function/CheckedCallableTest.java
@@ -0,0 +1,119 @@
+/*
+ * File:    CheckedCallableTest.java
+ * Package: commons.lambda.function
+ * Author:  Zachary Gill
+ * Repo:    https://github.com/ZGorlock/Java-Commons
+ */
+
+package commons.lambda.function;
+
+import commons.test.TestUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JUnit test of CheckedCallable.
+ *
+ * @see CheckedCallable
+ */
+@SuppressWarnings({"RedundantSuppression", "ConstantConditions", "unchecked", "SpellCheckingInspection"})
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CheckedCallable.class})
+public class CheckedCallableTest {
+    
+    //Logger
+    
+    /**
+     * The logger.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(CheckedCallableTest.class);
+    
+    
+    //Initialization
+    
+    /**
+     * The JUnit class setup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @BeforeClass
+    public static void setupClass() throws Exception {
+    }
+    
+    /**
+     * The JUnit class cleanup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @AfterClass
+    public static void cleanupClass() throws Exception {
+    }
+    
+    /**
+     * The JUnit setup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @Before
+    public void setup() throws Exception {
+    }
+    
+    /**
+     * The JUnit cleanup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @After
+    public void cleanup() throws Exception {
+    }
+    
+    
+    //Tests
+    
+    /**
+     * JUnit test of constants.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @Test
+    public void testConstants() throws Exception {
+    }
+    
+    /**
+     * JUnit test of tryCall.
+     *
+     * @throws Exception When there is an exception.
+     * @see CheckedCallable#tryCall()
+     */
+    @Test
+    public void testTryCall() throws Exception {
+        //standard
+        TestUtils.assertMethodExists(CheckedCallable.class, "tryCall");
+    }
+    
+    /**
+     * JUnit test of call.
+     *
+     * @throws Exception When there is an exception.
+     * @see CheckedCallable#call()
+     */
+    @Test
+    public void testCall() throws Exception {
+        //standard
+        TestUtils.assertMethodExists(CheckedCallable.class, "call");
+    }
+    
+}
