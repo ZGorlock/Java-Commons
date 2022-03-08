f28f89a9352aaa2c845882d661da86e2dc9461ff
Zachary Gill <zachary.w.gill@gmail.com>
2022-03-08 15:45:00 -0500

    Added CheckedConsumer

---
+++ src/commons/lambda/function/CheckedConsumer.java
@@ -0,0 +1,44 @@
+/*
+ * File:    CheckedConsumer.java
+ * Package: commons.lambda.function
+ * Author:  Zachary Gill
+ * Repo:    https://github.com/ZGorlock/Java-Commons
+ */
+
+package commons.lambda.function;
+
+import java.util.function.Consumer;
+
+/**
+ * A lambda function that tries to consume an input and returns no result and ignores errors.
+ *
+ * @param <T> The type of the input.
+ */
+@FunctionalInterface
+public interface CheckedConsumer<T> extends Consumer<T> {
+    
+    //Methods
+    
+    /**
+     * Tries to consume an input.
+     *
+     * @param arg The input.
+     * @throws Throwable When there is an error.
+     */
+    void tryAccept(T arg) throws Throwable;
+    
+    /**
+     * Tries to consume an input and ignores errors.
+     *
+     * @param arg the input.
+     * @see Consumer#accept(Object)
+     * @see #tryAccept(Object)
+     */
+    default void accept(T arg) {
+        try {
+            tryAccept(arg);
+        } catch (Throwable ignored) {
+        }
+    }
+    
+}
---
+++ test/commons/lambda/function/CheckedConsumerTest.java
@@ -0,0 +1,119 @@
+/*
+ * File:    CheckedConsumerTest.java
+ * Package: commons.lambda.function
+ * Author:  Zachary Gill
+ * Repo:    https://github.com/ZGorlock/Java-Commons
+ */
+
+package commons.lambda.function;
+
+import commons.test.TestUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JUnit test of CheckedConsumer.
+ *
+ * @see CheckedConsumer
+ */
+@SuppressWarnings({"RedundantSuppression", "ConstantConditions", "unchecked", "SpellCheckingInspection"})
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CheckedSupplier.class})
+public class CheckedConsumerTest {
+    
+    //Logger
+    
+    /**
+     * The logger.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(CheckedConsumerTest.class);
+    
+    
+    //Initialization
+    
+    /**
+     * The JUnit class setup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @BeforeClass
+    public static void setupClass() throws Exception {
+    }
+    
+    /**
+     * The JUnit class cleanup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @AfterClass
+    public static void cleanupClass() throws Exception {
+    }
+    
+    /**
+     * The JUnit setup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @Before
+    public void setup() throws Exception {
+    }
+    
+    /**
+     * The JUnit cleanup operations.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @After
+    public void cleanup() throws Exception {
+    }
+    
+    
+    //Tests
+    
+    /**
+     * JUnit test of constants.
+     *
+     * @throws Exception When there is an exception.
+     */
+    @SuppressWarnings("EmptyMethod")
+    @Test
+    public void testConstants() throws Exception {
+    }
+    
+    /**
+     * JUnit test of tryAccept.
+     *
+     * @throws Exception When there is an exception.
+     * @see CheckedConsumer#tryAccept(Object)
+     */
+    @Test
+    public void testTryAccept() throws Exception {
+        //standard
+        TestUtils.assertMethodExists(CheckedConsumer.class, "tryAccept", Object.class);
+    }
+    
+    /**
+     * JUnit test of accept.
+     *
+     * @throws Exception When there is an exception.
+     * @see CheckedConsumer#accept(Object)
+     */
+    @Test
+    public void testAccept() throws Exception {
+        //standard
+        TestUtils.assertMethodExists(CheckedConsumer.class, "accept", Object.class);
+    }
+    
+}
